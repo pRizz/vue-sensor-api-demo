09fe15b4bb78a21c3180750522e2520fe0d04f0c
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2019-12-20 22:20:46 -0600

    Show accelerometer data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
+/* eslint-disable */
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="text-align: center">
+    <div style="border-radius: 10px; border-width: 1px">
+      <div>
+        Accelerometer:
+      </div>
+      <div>
+        x: {{accelerometerData.x}}
+      </div>
+      <div>
+        y: {{accelerometerData.y}}
+      </div>
+      <div>
+        z: {{accelerometerData.z}}
+      </div>
+      <div>
+        Error: {{accelerometerError}}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +28,29 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      accelerometer: null,
+      accelerometerData: {},
+      accelerometerError: null,
+    }
+  },
+  methods: {
+    initAccelerometer() {
+      try {
+        this.accelerometer = new Accelerometer({frequency: 10})
+        this.accelerometer.addEventListener('reading', () => {
+          this.accelerometerData = {...this.accelerometer}
+        })
+      } catch(error) {
+        this.accelerometerError = error
+      }
+    }
+  },
+  mounted() {
+    console.log("test")
+    this.initAccelerometer()
   }
 }
 </script>
