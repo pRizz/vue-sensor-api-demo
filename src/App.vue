362903a228af470fc2e668d0c5a22482fa7d4a26
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2019-12-21 14:43:40 -0600

    Show more info

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,19 @@
   <div id="app" style="text-align: center">
     <div style="border-radius: 10px; border-width: 1px">
       <div>
-        Accelerometer:
+        Sensor Permissions: {{permissionsResult}}
+      </div>
+      <div>
+        Sensor Permissions Error: {{permissionsError}}
+      </div>
+      <div>
+        Sensor Permissions State: {{permissionsResult && permissionsResult.state}}
+      </div>
+      <div>
+        window.Accelerometer: {{getWindow().Accelerometer}}
+      </div>
+      <div>
+        accelerometer: {{accelerometer}}
       </div>
       <div>
         x: {{accelerometerData.x}}
@@ -15,7 +27,7 @@
         z: {{accelerometerData.z}}
       </div>
       <div>
-        Error: {{accelerometerError}}
+        accelerometerError: {{accelerometerError}}
       </div>
     </div>
   </div>
@@ -31,27 +43,42 @@ export default {
   },
   data() {
     return {
+      permissionsResult: null,
+      permissionsError: null,
       accelerometer: null,
       accelerometerData: {},
       accelerometerError: null,
     }
   },
   methods: {
+    getWindow() {
+      return window
+    },
     initAccelerometer() {
       try {
         this.accelerometer = new Accelerometer({frequency: 10})
         this.accelerometer.addEventListener('reading', () => {
           this.accelerometerData = {...this.accelerometer}
+          console.log(`${new Date().toISOString()}: got accelerometer reading: `, this.accelerometerData)
         })
         this.accelerometer.start()
       } catch(error) {
         this.accelerometerError = error
       }
+    },
+    async requestPermissions() {
+      this.permissionsResult = await navigator.permissions.query({name: "accelerometer"})
+      if(this.permissionsResult.state === 'denied') {
+        return
+      }
+      this.initAccelerometer()
     }
   },
   mounted() {
-    console.log("test")
-    this.initAccelerometer()
+    console.log("mounted")
+    this.requestPermissions().catch((error) => {
+      this.permissionsError = error
+    })
   }
 }
 </script>
